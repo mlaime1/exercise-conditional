a5cdb340875a853181f1856a234e69b5820be156
MauroLaime <maurol.dev@gmail.com>
2026-03-12 01:58:52 -0300

    feat: Implement a dynamic profile card with customizable fields and update build scripts for compatibility.

--- src/js/app.js
+++ src/js/app.js
@@ -33,14 +33,19 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name || "Lucy"} ${variables.lastName ||
+    "Boilett"}</h1>
+          <h2>${variables.role || "Web Developer"}</h2>
+          <h3>${variables.city || "Miami"}, ${variables.country || "USA"}</h3>
+          <ul class="${variables.socialMediaPosition || "position-right"}">
+            <li><a href="https://twitter.com/${variables.twitter ||
+              "4geeksacademy"}"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${variables.github ||
+              "4geeksacademy"}"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/school/${variables.linkedin ||
+              "4geeksacademy"}"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram ||
+              "4geeksacademy"}"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -48,6 +53,8 @@ function render(variables = {}) {
 
 /**
  * Don't change any of the lines below, here is where we do the logic for the dropdowns
+ *
+ *
  */
 window.onload = function() {
   window.variables = {
